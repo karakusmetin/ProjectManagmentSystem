652c17b194360c5f91ab2f1db773b9d27e6718b7
Metin Karakus <karakusmeetin@gmail.com>
2024-03-18 09:52:37 +0300

    The entity user has been added into the concrete folder

--- PMS-EntityLayer/Concrete/User.cs
+++ PMS-EntityLayer/Concrete/User.cs
@@ -2,6 +2,10 @@
 {
 	public class User
 	{
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
 
-	}
+    }
 }
