13af2b7d510fbfd82bd963376d4bb4927d3377d1
Metin Karakus <karakusmeetin@gmail.com>
2024-03-20 13:46:08 +0300

    A generic repository was created within the data layer, and basic methods were completed along with the interface class

---
+++ PMS.DataLayer/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using PMS.CoreLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PMS.DataLayer.Repositories.Abstracts
+{
+	public interface IRepository<T> where T : class,IBaseEntityWithId ,new()
+	{
+		Task AddAsync(T entity);
+		Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+		Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+		Task<T> GetByGuidAsync(Guid id);
+		Task<T> UpdateAsnyc(T entity);
+		Task<T> DeleteAsync(T entity);
+		Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+		Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+	}
+}
